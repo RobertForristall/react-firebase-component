752dc5db3ebe29b5c91eea3aa14765faaa81aa98
Robert Forristall <robert.s.forristall@gmail.com>
2026-03-28 19:07:01 -0400

    fix(dashboard): redirect user to login if they aren't logged in

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { createAuth, type FirebaseConfig } from "@/config/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, type User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface DashboardProps {
   firebaseConfig: FirebaseConfig;
@@ -7,16 +9,25 @@ interface DashboardProps {
 
 const Dashboard: React.FC<DashboardProps> = ({ firebaseConfig }) => {
   const auth = createAuth(firebaseConfig);
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // You can manage your app state (e.g., set a global user object in a Context/Redux store)
-      console.log("User is signed in:", user.uid);
-    } else {
-      // User is signed out
-      console.log("No user is signed in.");
-    }
-  });
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState<User | null>();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // You can manage your app state (e.g., set a global user object in a Context/Redux store)
+        console.log("User is signed in:", user.uid);
+        setUser(user);
+      } else {
+        // User is signed out
+        console.log("No user is signed in.");
+        navigate("/login");
+      }
+    });
+  }, []);
   return <></>;
 };
 
